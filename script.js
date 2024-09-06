b391f563bb1f7450692843914ba2a395b0125c81
NatanFreitas <115927951+MrNatan2023@users.noreply.github.com>
2024-09-06 17:50:14 -0300

    update in the jobcard

--- script.js
+++ script.js
@@ -1,31 +1,34 @@
-async function fetchJobs(params) {
-    try{
-        const reponse = await fetch('http://localhost:3000/jobs')
-        const jobs = await reponse.json()
-        
-        const jobContainer = document.querySelector('.job-container')
+async function fetchJobs() {
+    try {
+        const response = await fetch('http://localhost:3000/jobs'); 
+        const jobs = await response.json(); 
 
-        jobContainer.innerHTML = '';
+        const jobContainer = document.querySelector('.job-container'); 
+        jobContainer.innerHTML = ''; 
 
         jobs.forEach(job => {
-            const jobCard = document.createElement('div')
-            jobCard.classList.add('job-card')
+            const jobCard = document.createElement('div');
+            jobCard.classList.add('job-card');
 
             jobCard.innerHTML = `
-            <h3>${job.name_company}</h3>
-            <p><strong>Tipo de Emprego:</strong> ${job.type_of_employment}</p>
-            <p><strong>Qualificações:</strong> ${job.qualifications}</p>
-            <p><strong>Descrição:</strong> ${job.description}</p>
-            <p><strong>Benefícios:</strong> ${job.benefits}</p>
-        `;
-            jobContainer.appendChild(jobCard)
+                <p><strong>Nome da Companhia:</strong></p>
+                <h3>${job.name_company}</h3>
+                <p><strong>Tipo de Emprego:</strong></p>
+                <p>${job.type_of_employment}</p>
+                <p><strong>Qualificações:</strong></p>
+                <p>${job.qualifications}</p>
+                <p><strong>Descrição:</strong></p>
+                <p> ${job.description}</p>
+                <p><strong>Benefícios:</strong></p>
+                <p>${job.benefits}</p>
+            `;
 
+            jobContainer.appendChild(jobCard); 
         });
 
-
-    } catch(error){
-        console.log('Erro ao buscar vagas:', error)
-    }  
+    } catch (error) {
+        console.error('Erro ao buscar vagas:', error); 
+    }
 }
 
 document.addEventListener('DOMContentLoaded', fetchJobs);
--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const app = express();
 const port = 3000;
+app.use(cors());
 
 mongoose.connect('mongodb://localhost:27017/remotework', { useNewUrlParser: true, useUnifiedTopology: true })
 .then(() => console.log('MongoDB connect'))
